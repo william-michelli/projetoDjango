4f917f1456e5392f8c773ea90056c51455da9496
William Michelli <128925250+william-michelli@users.noreply.github.com>
2023-07-05 12:05:56 -0300

    Add files via upload

--- polls/views.py
+++ polls/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá, mundo. Você está no index da enquete")
+    return HttpResponse("Este é meu primeiro site utilizando django")
 
